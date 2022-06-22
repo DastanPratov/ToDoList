b2a975315f14b0355f4fc99d2a61370155a1ead7
Dastan <dastan@192.168.1.8>
2022-06-23 02:58:15 +0600

    first commit

---
+++ ToDoList/ToDoList/Model.swift
@@ -0,0 +1,41 @@
+//
+//  Model.swift
+//  ToDoList
+//
+//  Created by Dastan on 22/6/22.
+//
+
+import Foundation
+
+var toDoItems: [[String: Any]] {
+    set {
+        UserDefaults.standard.set(newValue, forKey: "ToDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    get {
+        if let array = UserDefaults.standard.array(forKey: "ToDoDataKey") as? [[String: Any]] {
+            return array
+        } else {
+            return []
+        }
+    }
+}
+
+func additem(nameItem: String, isCompleted: Bool = false) {
+    toDoItems.append(["Name": nameItem, "isCompleted": isCompleted])
+}
+
+func removeItem(at index: Int) {
+    toDoItems.remove(at: index)
+}
+
+func changeState(at item: Int) -> Bool {
+    toDoItems[item]["isCompleted"] = !(toDoItems[item]["isCompleted"] as! Bool)
+    return (toDoItems[item]["isCompleted"]) as! Bool
+}
+
+func moveItem(fromIndex: Int, toIndex: Int) {
+    let from = toDoItems[fromIndex]
+    toDoItems.remove(at: fromIndex)
+    toDoItems.insert(from, at: toIndex )
+}
---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Dastan on 22/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
